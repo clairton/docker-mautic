2411ae63113237436276d08342d49f6c0a4bbb83
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-09-15 16:29:27 -0300

    remove mysql_admin from entrypoint_mautic_web.sh

--- common/entrypoint_mautic_web.sh
+++ common/entrypoint_mautic_web.sh
@@ -2,11 +2,6 @@
 
 chown -R www-data:www-data /var/www/html/config /var/www/html/var/logs /var/www/html/docroot/media
 
-# wait untill the db is fully up before proceeding
-while [[ $(mysqladmin --host=$MAUTIC_DB_HOST --port=$MAUTIC_DB_PORT --user=$MAUTIC_DB_USER --password=$MAUTIC_DB_PASSWORD ping) != "mysqld is alive" ]]; do
-	sleep 1
-done
-
 # generate a local config file if it doesn't exist.
 # This is needed to ensure the db credentials can be prefilled in the UI, as env vars aren't taken into account.
 if [ ! -f /var/www/html/config/local.php ]; then
